1876e4b8631a5ad2b71b16892b80c1299e596c8c
Михаил <dontliem1@gmail.com>
2018-09-13 01:04:09 +0500

    Скрывает и показывает выполненные задания.

--- index.php
+++ index.php
@@ -96,8 +96,7 @@ $show_complete_tasks = rand(0, 1);
                     </nav>
 
                     <label class="checkbox">
-                        <!--добавить сюда аттрибут "checked", если переменная $show_complete_tasks равна единице-->
-                        <input class="checkbox__input visually-hidden show_completed" type="checkbox">
+                        <input class="checkbox__input visually-hidden show_completed" type="checkbox" <?php if($show_complete_tasks === 1) {print ("checked");};?>/>
                         <span class="checkbox__text">Показывать выполненные</span>
                     </label>
                 </div>
@@ -117,8 +116,7 @@ $show_complete_tasks = rand(0, 1);
 
                         <td class="task__date"></td>
                     </tr>
-                    <!--показывать следующий тег <tr/>, если переменная $show_complete_tasks равна единице-->
-                    <tr class="tasks__item task task--completed">
+                    <tr class="tasks__item task task--completed" <?php if($show_complete_tasks === 1) {print ("hidden");};?>>
                         <td class="task__select">
                             <label class="checkbox task__checkbox">
                                 <input class="checkbox__input visually-hidden task__checkbox" type="checkbox" checked>
