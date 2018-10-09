9146269390cff48fffe1dc9c9ecaf621dc80561d
Михаил <dontliem1@gmail.com>
2018-10-09 23:39:25 +0500

    Поменял enctype формы регистрации

--- index.php
+++ index.php
@@ -2,20 +2,58 @@
 $link = mysqli_connect('localhost', 'root', '', 'doingsdone');
 mysqli_set_charset($link, "utf8");
 
-require_once('functions.php');
-require_once('data.php');
+$projects_list = [];
+$tasks_list_all = [];
+$tasks_list = [];
 
-$page_content = include_template('index.php',[
-    'tasks_list' => $tasks_list,
-    'show_complete_tasks' => $show_complete_tasks
-    ]);
+session_start();
+if ($link && isset($_SESSION['user'])) {
+    $cur_user = $_SESSION['user']['id'];
+
+    //Получаем список всех проектов этого пользователя
+    $sql = 'SELECT * FROM projects WHERE user_id = '.$_SESSION['user']['id'];
+    $result = mysqli_query($link, $sql);
+    if ($result) {
+        $projects_list = mysqli_fetch_all($result, MYSQLI_ASSOC);
+    }
+
+    //Получаем список всех задач этого пользователя и сортируем по новизне
+    $sql = 'SELECT * FROM tasks WHERE user_id = '.$_SESSION['user']['id'].' ORDER BY date_created DESC';
+    $result = mysqli_query($link, $sql);
+    if ($result) {
+        $tasks_list_all = mysqli_fetch_all($result, MYSQLI_ASSOC);
+        $tasks_list =  $tasks_list_all;
+    }
+
+    //При наличии параметра project, получаем список всех задач для одного проекта и сортируем по новизне
+    if (isset($_GET['project'])) {
+        $cur_project = $_GET['project'];
+        //Приводим параметр к целочисленному типу для защиты от SQL-инъекций
+        settype($cur_project, 'integer');
+        $project_ids = array_column($projects_list, 'id');
+
+        $sql = 'SELECT * FROM tasks WHERE user_id = '.$_SESSION['user']['id'].' AND project_id= '.$cur_project.' ORDER BY date_created DESC';
+        $result = mysqli_query($link, $sql);
+        if ($result) {
+            $tasks_list = mysqli_fetch_all($result, MYSQLI_ASSOC);
+        }
+        //Если параметр project пуст, либо если по этому id у пользователя не нашли ни одного проекта, то вместо содержимого страницы возвращаем код ответа 404
+        if (!in_array($cur_project, $project_ids) || empty($cur_project)) {
+            header("HTTP/1.1 404 Not Found");
+            die();
+        }
+    }
+}
+
+require_once('functions.php');
+
+if (isset($_SESSION['user'])) {$page_content = include_template('index.php',['tasks_list' => $tasks_list]);}
+else {$page_content = include_template('guest.php',[]);}
+
 $layout_content = include_template('layout.php',[
     'content' => $page_content,
-    'authorized' => $authorized,
     'projects_list' => $projects_list,
     'tasks_list_all' => $tasks_list_all,
-    'tasks_list' => $tasks_list,
-    'user' => esc($user_info['name']),
     'title' => 'Дела в порядке'
     ]);
 
--- templates/auth.php
+++ templates/auth.php
@@ -1,9 +1,5 @@
-<h2 class="content__main-heading">Регистрация аккаунта</h2>
+<h2 class="content__main-heading">Вход на сайт</h2>
 <? if (isset($errors['sql'])) {print ($errors['sql']);}?>
-<?php
-    $url = "$_SERVER[HTTP_HOST]";
-    $url_parts = parse_url($url);
-    var_dump(parse_url($url)); ?>
 <form class="form"  action="" method="post">
 	<div class="form__row">
 		<label class="form__label" for="email">E-mail <sup>*</sup></label>
@@ -21,16 +17,8 @@
       <?php if (isset($errors['password'])) {print ('<p class="form_message"><span class="form__message error-message">'.$errors['password'].'</span></p>');}?>
 	</div>
 
-	<div class="form__row">
-		<label class="form__label" for="name">Имя <sup>*</sup></label>
-      <?php $classname = isset($errors['name']) ? "form__input--error" : "";
-      $value = isset($form['name']) ? $form['name'] : ""; ?>
-		<input class="form__input <?=$classname;?>" type="text" name="name" id="name" value="<?=$value;?>" placeholder="Введите имя">
-		<?php if (isset($errors['name'])) {print ('<p class="form_message"><span class="form__message error-message">'.$errors['name'].'</span></p>');}?>
-	</div>
-
 	<div class="form__row form__row--controls">
 <?php if (!empty($errors)) {print ('<p class="error-message">Пожалуйста, исправьте ошибки в форме</p>');}?>
-		<input class="button" type="submit" name="" value="Зарегистрироваться">
+		<input class="button" type="submit" name="" value="Войти">
 	</div>
 </form>
